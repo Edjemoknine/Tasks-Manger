954b17a8e9b2344c00df3b384c7f8ff975d17764
moknine elhadj <mokninemoknine999@gmail.com>
2024-09-30 10:30:11 +0100

    last commit and force pushing

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/task"],
+  publicRoutes: ["/api/task", "/"],
 });
 
 export const config = {
